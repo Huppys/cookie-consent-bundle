16dc20130ad7b4f710f2edd4999e0a4a0b89b6c4
Oliver Witzki <oliver.witzki@chrono24.com>
2024-07-08 22:48:07 +0200

    Update test name, add empty test

--- tests/Controller/CookieConsentControllerKernelTest.php
+++ tests/Controller/CookieConsentControllerKernelTest.php
@@ -8,12 +8,18 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class CookieConsentControllerKernelTest extends WebTestCase
 {
     #[Test]
-    public function shouldBlockGetRequest(): void
+    public function shouldRenderTemplateOnRequest(): void
     {
         $client = static::createClient();
 
         $client->request('GET', '/cookie-consent/view');
 
         $this->assertResponseIsSuccessful();
+    }
+
+    #[Test]
+    public function shouldBlockGetRequestForUpdatingConsentSettings(): void
+    {
+        $this->markTestIncomplete();
     }
 }
